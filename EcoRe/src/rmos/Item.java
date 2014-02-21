969c21a02a3d36e88370e501ea8f9ddc97f84f5c
Kelsey Marshman <kels.marshman@gmail.com>
2014-02-21 14:52:36 -0800

    item and manager classes

--- EcoRe/src/rmos/Item.java
+++ EcoRe/src/rmos/Item.java
@@ -1,5 +1,37 @@
 package rmos;
 
 public class Item {
+	
+	private String name;
+	private ItemType type;
+	private double weight;
+	
+	public Item(String name, ItemType type, double weight){
+		this.name = name;
+		this.type = type;
+		this.weight = weight;
+	}
+	
+	public Item(){
+		name = "not set";
+		type = null;
+		weight = 0;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public ItemType getType(){
+		return type;
+	}
+	
+	public double getWeight(){
+		return weight;
+	}
+	
+	public double getValue(){
+		return weight * type.getPrice();
+	}
 
 }
--- EcoRe/src/rmos/ItemType.java
+++ EcoRe/src/rmos/ItemType.java
@@ -2,4 +2,32 @@ package rmos;
 
 public class ItemType {
 
+	private String name;
+	private double price;
+	
+	public ItemType(String name, double price){
+		this.name = name;
+		this.price = price;
+	}
+	
+	public ItemType(){
+		name = "not set";
+		price = 0;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public double getPrice(){
+		return price;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setPrice(double price){
+		this.price = price;
+	}
 }
